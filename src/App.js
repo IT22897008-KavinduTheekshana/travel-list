c9b17ba36e294808c30ee850d57f48dc95a1ab6f
Kavindu Theekshana <it22897008@my.sliit.lk>
2025-02-10 14:50:26 +0530

    Controlled Elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,16 +21,35 @@ function Logo() {
   return <h1>🏕️ Far Away 💼</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    const form = e.target;
-    const { quantity, item } = form.elements;
-    console.log(quantity.value, item.value);
+    if (!description) return;
+
+    const newItem = {
+      id: Math.random(),
+      description,
+      quantity,
+      packed: false,
+    };
+
+    setDescription("");
+    setQuantity(1);
   }
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
+    <form
+      className="add-form"
+      onSubmit={handleSubmit}
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}
+    >
       <h3>what do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -40,6 +61,7 @@ function Form() {
     </form>
   );
 }
+
 function PackingList() {
   return (
     <div className="list">
