bf6e95048e9d4f0719f15de93bcfe01576b337d4
Kavindu Theekshana <it22897008@my.sliit.lk>
2025-02-10 11:04:20 +0530

    building form and handling submission

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -13,15 +19,51 @@ function Logo() {
   return <h1>🏕️ Far Away 💼</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const { quantity, item } = form.elements;
+    console.log(quantity.value, item.value);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
-  return <div className="list"> List </div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
